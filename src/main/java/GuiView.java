53f023d10bf53a44d441ce263bf60d39d6accd6f
OmarEbrahim199 <70900248+OmarEbrahim199@users.noreply.github.com>
2021-01-08 16:29:56 +0100

    Commit

--- src/main/java/GuiView.java
+++ src/main/java/GuiView.java
@@ -46,6 +46,52 @@ public class GuiView {
         return fields;
     }
 
+
+
+    /*
+    fields[0] = new ModelStart("start",null);
+        fields[1] = new ModelEstate("rødovrevej",Color.BLUE,1200, new int[]{50,250,750,2250,4000,6000}, 1000);
+        fields[2] = new ModelChanceField("prøvLykken", null);
+        fields[3] = new ModelEstate("hvidovrevej",Color.BLUE,1200, new int[]{50,250,750,2250,4000,6000}, 1000);
+        fields[4] = new ModelIncomeTax("betalIndkomstskat",null, 4000);
+        fields[5] = new ModelFerry("øresund",Color.ORANGE,4000,500);
+        fields[6] = new ModelEstate("roskildevej",Color.PINK,2000,new int[]{100,600,1800,5400,8000,11000}, 1000);
+        fields[7] = new ModelChanceField("prøvLykken", null);
+        fields[8] = new ModelEstate("valbyLanggade",Color.PINK,2000,new int[]{100,600,1800,5400,8000,11000}, 1000);
+        fields[9] = new ModelEstate("allegade",Color.PINK,2400,new int[]{150,800,2000,6000,9000,12000}, 1000);
+        fields[10] = new ModelVisitJail("fængsel",null);
+        fields[11] = new ModelEstate("frederiksbergsAlle",Color.GREEN,2800,new int[]{200,1000,3000,9000,12500,15000}, 2000);
+        fields[12] = new ModelBrewery("tuborg",new Color(152,251,152),3000,100);
+        fields[13] = new ModelEstate("bulowsvej",Color.GREEN,2800,new int[]{200,1000,3000,9000,12500,15000}, 2000);
+        fields[14] = new ModelEstate("gammelKongevej",Color.GREEN,3200,new int[]{250,1250,3750,10000,14000,18000}, 2000);
+        fields[15] = new ModelFerry("dFDS",Color.ORANGE,4000,500);
+        fields[16] = new ModelEstate("bernstorffsvej", Color.GRAY, 3600,new int[]{300,1400,4000,11000,15000,19000},2000);
+        fields[17] = new ModelChanceField("prøvLykken", null);
+        fields[18] = new ModelEstate("hellerupvej", Color.GRAY, 3600,new int[]{300,1400,4000,11000,15000,19000},2000);
+        fields[19] = new ModelEstate("strandvejen", Color.GRAY, 4000,new int[]{350,1600,4400,12000,16000,20000},2000);
+        fields[20] = new ModelParking("helle",null);
+        fields[21] = new ModelEstate("trianglen",Color.RED,4400,new int[]{350,1800,5000,14000,17500,21000},3000);
+        fields[22] = new ModelChanceField("prøvLykken",null);
+        fields[23] = new ModelEstate("østerbrogade",Color.RED,4400,new int[]{350,1800,5000,14000,17500,21000},3000);
+        fields[24] = new ModelEstate("grønningen",Color.RED,4800,new int[]{400,2000,6000,15000,18500,22000},3000);
+        fields[25] = new ModelFerry("øS",Color.ORANGE,4000,500);
+        fields[26] = new ModelEstate("bredgade",Color.CYAN,5200,new int[]{450,2200,6600,16000,19500,23000},3000);
+        fields[27] = new ModelEstate("kgsNytorv",Color.CYAN,5200,new int[]{450,2200,6600,16000,19500,23000},3000);
+        fields[28] = new ModelBrewery("carlsberg", new Color(152,251,152),3000,100);
+        fields[29] = new ModelEstate("østergade",Color.CYAN,5600,new int[]{500,2400,7200,17000,20500,24000},3000);
+        fields[30] = new ModelGotoJail("gåIFængsel",null);
+        fields[31] = new ModelEstate("amagertorv",Color.YELLOW,6000,new int[]{550,2600,7800,18000,22000,25000}, 4000);
+        fields[32] = new ModelEstate("vimmelskaftet",Color.YELLOW,6000,new int[]{550,2600,7800,18000,22000,25000}, 4000);
+        fields[33] = new ModelChanceField("prøvLykken",null);
+        fields[34] = new ModelEstate("nygade",Color.YELLOW,6400,new int[]{600,3000,9000,20000,24000,28000}, 4000);
+        fields[35] = new ModelFerry("bornholm",Color.ORANGE,4000,500);
+        fields[36] = new ModelChanceField("prøvLykken",null);
+        fields[37] = new ModelEstate("frederiksberggade",Color.MAGENTA,7000,new int[]{700,3500,10000,22000,26000,30000},4000);
+        fields[38] = new ModelIncomeTax("ekstraordinærStatsskat",null, 2000);
+        fields[39] = new ModelEstate("rådhuspladsen",Color.MAGENTA,8000,new int[]{1000,4000,12000,28000,34000,40000},4000);
+    }
+     */
+
     public GUI_Car vehicleChoice(int playerId) {
         String vehicle = gui.getUserSelection("Vælg din transport", "Bil", "Traktor", "UFO");
         Color color = getVehicleColor(playerId);
